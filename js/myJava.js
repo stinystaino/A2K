85037841bb4042d8335f5e6dc76a4738555b754e
Kanjanda <osten.kanjanda@liberty.co.za>
2019-08-02 21:11:33 +0200

    First commit

---
+++ js/myJava.js
@@ -0,0 +1,273 @@
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $("nav").addClass("black"); 
+    } else {
+        $("nav").removeClass("black");
+    }
+});
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate({
+                    scrollTop: $(hash).offset().top
+                },
+                1000,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
+});
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
+
+
+
+
+
+
+
+
+
+(function ($) {
+    "use strict";
+
+
+
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#wrap").lsPreloader({
+
+});
+
+
+$("#wrap").lsPreloader({
+
+ 
+
+// background color
+
+backgroundColor:  "#000",
+
+ // background image
+
+backgroundImage:  "",  
+
+ // repeat | no-repeat | repeat-x | repeat-y
+
+backgroundRepeat: "repeat",
+
+ // display your logo    
+
+logoImage:      "",  
+
+ // display progress bar
+
+progressShow:   true,
+
+ 
+// progress bar color
+
+progressColor:    "#FFF",
+
+ 
+
+// progress bar height
+
+progressHeight:   "10px",
+
+ 
+
+// top | center | bottom
+
+progressPosition: "bottom",
+
+ 
+
+// display percentage
+
+percentShow:    true,     
+
+ 
+
+// font family
+
+percentFontFamily:  "Verdana, Geneva, sans-serif",
+
+ 
+
+// font size
+
+percentFontSize:  "50px", 
+
+ 
+
+// choose an animation type
+
+// fade | hide | slideUp | slideDown | slideLeft | slideRight 
+
+animationComplete:  "fade",
+
+ 
+
+// minimum time of execution in seconds
+
+minimumTime:    .5,         
+
+ 
+
+// callbacks      
+
+onStart:      function(){},
+
+onComplete:     function(){}
+
+ 
+
+});
+
+
